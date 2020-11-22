545ef3d8be13d7e536e552f69ed80d3eb09836a0
ithansiyeon <itsiyeon@nate.com>
2020-11-22 19:58:38 +0900

    commit

--- Algorithm/src/com/algorithm/programmers/Programmers_Solution6.java
+++ Algorithm/src/com/algorithm/programmers/Programmers_Solution6.java
@@ -1,79 +1,68 @@
 package com.algorithm.programmers;
-/*땅따먹기 게임을 하려고 합니다. 땅따먹기 게임의 땅(land)은 총 N행 4열로 이루어져 있고, 모든 칸에는 점수가 쓰여 있습니다. 
-  
-  1행부터 땅을 밟으며 한 행씩 내려올 때, 각 행의 4칸 중 한 칸만 밟으면서 내려와야 합니다. 
-  
-  단, 땅따먹기 게임에는 한 행씩 내려올 때, 같은 열을 연속해서 밟을 수 없는 특수 규칙이 있습니다.
 
-예를 들면,
+import java.util.Collections;
+import java.util.PriorityQueue;
 
-| 1 | 2 | 3 | 5 |
+//일반적인 프린터는 인쇄 요청이 들어온 순서대로 인쇄합니다. 그렇기 때문에 중요한 문서가 나중에 인쇄될 수 있습니다. 이런 문제를 보완하기 위해 중요도가 높은 문서를 먼저 인쇄하는 프린터를 개발했습니다. 이 새롭게 개발한 프린터는 아래와 같은 방식으로 인쇄 작업을 수행합니다.
+//
+//1. 인쇄 대기목록의 가장 앞에 있는 문서(J)를 대기목록에서 꺼냅니다.
+//2. 나머지 인쇄 대기목록에서 J보다 중요도가 높은 문서가 한 개라도 존재하면 J를 대기목록의 가장 마지막에 넣습니다.
+//3. 그렇지 않으면 J를 인쇄합니다.
+//예를 들어, 4개의 문서(A, B, C, D)가 순서대로 인쇄 대기목록에 있고 중요도가 2 1 3 2 라면 C D A B 순으로 인쇄하게 됩니다.
+//
+//내가 인쇄를 요청한 문서가 몇 번째로 인쇄되는지 알고 싶습니다. 위의 예에서 C는 1번째로, A는 3번째로 인쇄됩니다.
+//
+//현재 대기목록에 있는 문서의 중요도가 순서대로 담긴 배열 priorities와 내가 인쇄를 요청한 문서가 현재 대기목록의 어떤 위치에 있는지를 알려주는 location이 매개변수로 주어질 때, 내가 인쇄를 요청한 문서가 몇 번째로 인쇄되는지 return 하도록 solution 함수를 작성해주세요.
+//
+//제한사항
+//현재 대기목록에는 1개 이상 100개 이하의 문서가 있습니다.
+//인쇄 작업의 중요도는 1~9로 표현하며 숫자가 클수록 중요하다는 뜻입니다.
+//location은 0 이상 (현재 대기목록에 있는 작업 수 - 1) 이하의 값을 가지며 대기목록의 가장 앞에 있으면 0, 두 번째에 있으면 1로 표현합니다.
+//입출력 예
+//priorities	location	return
+//[2, 1, 3, 2]	2	1
+//[1, 1, 9, 1, 1, 1]	0	5
+//입출력 예 설명
+//예제 #1
+//
+//문제에 나온 예와 같습니다.
+//
+//예제 #2
+//
+//6개의 문서(A, B, C, D, E, F)가 인쇄 대기목록에 있고 중요도가 1 1 9 1 1 1 이므로 C D E F A B 순으로 인쇄합니다.
 
-| 5 | 6 | 7 | 8 |
-
-| 4 | 3 | 2 | 1 |
-
-로 땅이 주어졌다면, 1행에서 네번째 칸 (5)를 밟았으면, 2행의 네번째 칸 (8)은 밟을 수 없습니다.
-
-마지막 행까지 모두 내려왔을 때, 얻을 수 있는 점수의 최대값을 return하는 solution 함수를 완성해 주세요. 
-위 예의 경우, 1행의 네번째 칸 (5), 2행의 세번째 칸 (7), 3행의 첫번째 칸 (4) 땅을 밟아 16점이 최고점이 되므로 16을 return 하면 됩니다.
-
-제한사항
-행의 개수 N : 100,000 이하의 자연수
-열의 개수는 4개이고, 땅(land)은 2차원 배열로 주어집니다.
-점수 : 100 이하의 자연수
-입출력 예
-land	answer
-[[1,2,3,5],[5,6,7,8],[4,3,2,1]]	16
-입출력 예 설명
-입출력 예 #1
-문제의 예시와 같습니다.*/
-
-public class Programmers_Solution6 {
+public class Programmers_Solution7 {
 	public static void main(String[] args) {
-		Solution1 s = new Solution1();
-		int[][] list = { { 1, 2, 3, 5 }, { 5, 6, 7, 8 }, { 4, 3, 2, 1 } };
-		s.solution(list);
+		int [] priorities = {1, 1, 9, 1, 1, 1};
+		int answer = solution(priorities,0);
+		System.out.println(answer);
 	}
-}
-
-class Solution1 {
-	int solution(int[][] land) {
-		int answer = 0;
-		int row = land.length;
-		int col = land[0].length;
-//		System.out.println(row);
-//		System.out.println(col);
-		int max = -10;
-		int before = -1;
-		int after = -1;
-		int sum = 0;
-		// [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-//		System.out.println("Hello Java");
-		int j = 0;
 	
-		for (int i = 0; i < row; i++) {
-			after = before;
-//			System.out.printf("max : %d  before :%d after: %d\r\n", max, before, after);
-			for (j = 0; j < col; j++) {
-				before = -1;
-				if (land[i][j] > max) {
-					if (i == 0) {
-						max = land[i][j];
-						before = j;
-					} else if (i > 0 && j == after) {
+	public static int solution(int[] priorities, int location) {
+        int answer = 1;
 
-					} else if (i > 0 && j != after) {
-						max = land[i][j];
-						before = j;
-					}
-				} 
-			}
-		
-			sum += max;
-			max = -10;
-		}
-//		System.out.println(sum);
-		return sum;
-	}
+        PriorityQueue priority = new PriorityQueue<>(Collections.reverseOrder());
+
+        for(int task : priorities){
+            priority.add(task);
+            System.out.println(priority);
+        }
+        //{2,5,4,1,3};
+
+        System.out.println(priority);
+        while(!priority.isEmpty()){
+            for(int i=0; i<priorities.length; i++){
+                if(priorities[i] == (int)priority.peek()) {
+                    if(i == location){
+                        return answer;
+                    }
+                    priority.poll();
+                    answer++;
+                }
+            }
+        }
+
+        return answer;
+    }
+	
 }
--- Algorithm/src/com/algorithm/programmers/Programmers_Solution7.java
+++ Algorithm/src/com/algorithm/programmers/Programmers_Solution7.java
@@ -1,67 +1,47 @@
 package com.algorithm.programmers;
 
-import java.util.Collections;
-import java.util.PriorityQueue;
+import java.util.ArrayList;
+import java.util.Arrays;
 
-//일반적인 프린터는 인쇄 요청이 들어온 순서대로 인쇄합니다. 그렇기 때문에 중요한 문서가 나중에 인쇄될 수 있습니다. 이런 문제를 보완하기 위해 중요도가 높은 문서를 먼저 인쇄하는 프린터를 개발했습니다. 이 새롭게 개발한 프린터는 아래와 같은 방식으로 인쇄 작업을 수행합니다.
+//배열 arr가 주어집니다. 배열 arr의 각 원소는 숫자 0부터 9까지로 이루어져 있습니다. 이때, 배열 arr에서 연속적으로 나타나는 숫자는 하나만 남기고 전부 제거하려고 합니다. 단, 제거된 후 남은 수들을 반환할 때는 배열 arr의 원소들의 순서를 유지해야 합니다. 예를 들면,
 //
-//1. 인쇄 대기목록의 가장 앞에 있는 문서(J)를 대기목록에서 꺼냅니다.
-//2. 나머지 인쇄 대기목록에서 J보다 중요도가 높은 문서가 한 개라도 존재하면 J를 대기목록의 가장 마지막에 넣습니다.
-//3. 그렇지 않으면 J를 인쇄합니다.
-//예를 들어, 4개의 문서(A, B, C, D)가 순서대로 인쇄 대기목록에 있고 중요도가 2 1 3 2 라면 C D A B 순으로 인쇄하게 됩니다.
-//
-//내가 인쇄를 요청한 문서가 몇 번째로 인쇄되는지 알고 싶습니다. 위의 예에서 C는 1번째로, A는 3번째로 인쇄됩니다.
-//
-//현재 대기목록에 있는 문서의 중요도가 순서대로 담긴 배열 priorities와 내가 인쇄를 요청한 문서가 현재 대기목록의 어떤 위치에 있는지를 알려주는 location이 매개변수로 주어질 때, 내가 인쇄를 요청한 문서가 몇 번째로 인쇄되는지 return 하도록 solution 함수를 작성해주세요.
+//arr = [1, 1, 3, 3, 0, 1, 1] 이면 [1, 3, 0, 1] 을 return 합니다.
+//arr = [4, 4, 4, 3, 3] 이면 [4, 3] 을 return 합니다.
+//배열 arr에서 연속적으로 나타나는 숫자는 제거하고 남은 수들을 return 하는 solution 함수를 완성해 주세요.
 //
 //제한사항
-//현재 대기목록에는 1개 이상 100개 이하의 문서가 있습니다.
-//인쇄 작업의 중요도는 1~9로 표현하며 숫자가 클수록 중요하다는 뜻입니다.
-//location은 0 이상 (현재 대기목록에 있는 작업 수 - 1) 이하의 값을 가지며 대기목록의 가장 앞에 있으면 0, 두 번째에 있으면 1로 표현합니다.
-//입출력 예
-//priorities	location	return
-//[2, 1, 3, 2]	2	1
-//[1, 1, 9, 1, 1, 1]	0	5
-//입출력 예 설명
-//예제 #1
-//
-//문제에 나온 예와 같습니다.
-//
-//예제 #2
-//
-//6개의 문서(A, B, C, D, E, F)가 인쇄 대기목록에 있고 중요도가 1 1 9 1 1 1 이므로 C D E F A B 순으로 인쇄합니다.
+//배열 arr의 크기 : 1,000,000 이하의 자연수
+//배열 arr의 원소의 크기 : 0보다 크거나 같고 9보다 작거나 같은 정수
 
-public class Programmers_Solution7 {
+public class Programmers_Solution8 {
 	public static void main(String[] args) {
-		int [] priorities = {1, 1, 9, 1, 1, 1};
-		int answer = solution(priorities,0);
-		System.out.println(answer);
+		int [] nums = {1,1,3,3,0,1,1};
+		int [] answer = solution(nums);
+		System.out.println(Arrays.toString(answer));
 	}
-	
-	public static int solution(int[] priorities, int location) {
-        int answer = 1;
 
-        PriorityQueue priority = new PriorityQueue<>(Collections.reverseOrder());
-
-        for(int task : priorities){
-            priority.add(task);
-            System.out.println(priority);
+    public static int[] solution(int []arr) {
+    	
+    	ArrayList<Integer> list = new ArrayList<Integer>();
+    	
+        for(int i=0;i<arr.length;i++) {
+        	if(i==0) {
+        		list.add(arr[i]);
+        	}
+    		if(i!=0) {
+    			if(arr[i-1]!=arr[i]) {
+        			list.add(arr[i]);
+        		}
+    			
+    		}
+    		
+        	
         }
-        //{2,5,4,1,3};
-
-        System.out.println(priority);
-        while(!priority.isEmpty()){
-            for(int i=0; i<priorities.length; i++){
-                if(priorities[i] == (int)priority.peek()) {
-                    if(i == location){
-                        return answer;
-                    }
-                    priority.poll();
-                    answer++;
-                }
-            }
+        int[] answer = new int[list.size()];
+        for(int i=0;i<list.size();i++) {
+        	answer[i] = list.get(i);
         }
-
+        
         return answer;
     }
 	
